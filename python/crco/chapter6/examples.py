ef3bb6792d58a3e24e3f786aae0c99094f7e02df
Carlos Aguilera <a.carlosing@gmail.com>
2020-08-16 16:54:01 -0500

    CRCO: Fib & Fib cached.

--- python/crco/chapter6/examples.py
+++ python/crco/chapter6/examples.py
@@ -1,6 +1,8 @@
 import math
 
 # Example 1 - Time complexity O(N).
+
+
 def foo(a):
     sum = 0
     product = 1
@@ -14,24 +16,32 @@ def foo(a):
     print(sum, ',', product)
 
 # Example 2 - Time complexity O(N^2).
+
+
 def printPairs(a):
     for i in range(0, len(a)):
         for j in range(0, len(a)):
             print(a[i], ',', a[j])
 
 # Example 3 - Time complexity O(N^2).
+
+
 def printPairs2(a):
     for i in range(0, len(a)):
         for j in range(i + 1, len(a)):
             print(a[i], ',', a[j])
 
 # Example 4 - Time complexity O(AB).
+
+
 def printUnorderedPairs(a, b):
     for i in range(len(a)):
         for j in range(len(b)):
             print(a[i], ',', a[j])
 
 # Example 5 - Time complexity O(AB * 100). We can drop the constants => O(AB)
+
+
 def printUnorderedPairs2(a, b):
     for i in range(len(a)):
         for j in range(len(b)):
@@ -39,13 +49,16 @@ def printUnorderedPairs2(a, b):
                 print(a[i], ',', a[j])
 
 # Example 6 - Time complexity O(N).
+
+
 def reverse(a):
     for i in range(len(a) // 2):
-        other = len(a) - i -1
+        other = len(a) - i - 1
         temp = a[i]
-        a[i]  =a[other]
+        a[i] = a[other]
         a[other] = temp
     return a
+
 
 # Example 8
 """
@@ -71,11 +84,14 @@ a["aa", "ab", "acz"]
 """
 
 # Example 9
+
+
 class Node:
     def __init__(self, value):
         self.value = value
         self.right = None
         self.left = None
+
 
 '''
 Recurseive function with multiple branches is typically O(branches^depth)
@@ -93,12 +109,16 @@ Let P = 2^log N
 
 
 '''
+
+
 def sum(node):
     if node == None:
         return 0
     return sum(node.left) + node.value + sum(node.right)
 
 # Example 10 - Time complexity O(sqrt(N)).
+
+
 def isPrime(n):
     x = 2
     while x * x <= math.sqrt(n):
@@ -108,6 +128,8 @@ def isPrime(n):
     return True
 
 # Example 11 - Time complexity O(sqrt(N)).
+
+
 def factorial(n):
     if n < 0:
         return -1
@@ -116,9 +138,13 @@ def factorial(n):
     else:
         return n * factorial(n - 1)
 
-# Example 12 - Time complexity
+# Example 12 - Time complexity: O(N^2 * N!)
+
+
 def permutations(str):
     permutationsAux(str, "")
+
+
 def permutationsAux(str, prefix):
     if len(str) == 0:
         print(prefix)
@@ -127,11 +153,48 @@ def permutationsAux(str, prefix):
             rem = str[:i] + str[i + 1:]
             permutationsAux(rem, prefix + str[i])
 
+# Example 13 - Time complexity: O(2^N)
 
-data = [1,2,3,4,5,6,7,8]
+
+def fib(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    return fib(n - 1) + fib(n - 2)
+
+# Example 14 - Time complexity: O(2^N+1) => O(2^N)
+def allFib(n):
+    for i in range(n):
+        print(fib(i))
+
+# Example 15 - Time complexity: O(N)
+def allFibCached(n):
+    cache = [0] * n
+    for i in range(n):
+        print(fibCached(i, cache))
+
+def fibCached(n, cache):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    elif cache[n] > 0:
+        return cache[n]
+    cache[n] = fibCached(n - 1, cache) + fibCached(n - 2, cache)
+    return cache[n]
+
+data = [1, 2, 3, 4, 5, 6, 7, 8]
 root = Node(3)
 root.left = Node(1)
 root.right = Node(2)
+
+print("allFibCached")
+allFibCached(20)
+
+print("allFib")
+allFib(20)
+print(fib(20))
 
 permutations("abc")
 print(factorial(3))
