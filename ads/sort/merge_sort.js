730f6cd77959d40e923fea018fd22804efbf9034
Carlos Aguilera <a.carlosing@gmail.com>
2020-09-26 12:11:18 -0500

    ADS: Merge sort

---
+++ ads/sort/merge_sort.js
@@ -0,0 +1,39 @@
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    let middle = Math.floor(arr.length / 2);
+    let left = mergeSort(arr.slice(0, middle));
+    let right = mergeSort(arr.slice(middle));
+    return merge(left, right);
+}
+
+function merge(arr1, arr2) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr2[j] > arr1[i]) {
+            result.push(arr1[i]);
+            i++;
+        } else {
+            result.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        result.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        result.push(arr2[j]);
+        j++;
+    }
+
+    return result;
+}
+
+console.log(mergeSort([5, 4, 3, 2, 1]).toString() == [1, 2, 3, 4, 5].toString());
