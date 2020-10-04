9acf124385c7b99c33e26c4546713589a00a776c
Carlos Aguilera <a.carlosing@gmail.com>
2020-10-04 18:35:30 -0500

    ADS: Dounly Linked List push.

--- ads/datastructures/DoublyLinkedList.js
+++ ads/datastructures/DoublyLinkedList.js
@@ -6,4 +6,32 @@ class DoublyLinkedList {
         this.tail = null;
         this.length = 0;
     }
+
+    push(value) {
+        let newNode = new Node(value);
+        if (this.length === 0) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+        this.length += 1;
+        return this;
+    }
+
+    toString() {
+        let current = this.head
+        let list = '';
+        while (current !== null) {
+            list += `${current.value} ${current.next ? '<-> ' : '-> '}`;
+            current = current.next;
+        }
+
+        list += `null`;
+        console.log(list);
+    }
 }
+
+export default DoublyLinkedList;
--- ads/datastructures/main.js
+++ ads/datastructures/main.js
@@ -1,7 +1,12 @@
 import LinkeList from './LinkedList.js';
+import DoublyLinkedList from './DoublyLinkedList.js';
 
+/**
+ * LinkedList operations
+ */
 let linkedList = new LinkeList();
-linkedList.push(1).push(2).push(3);
+console.log('LinkedList operations');
+linkedList.push(1).push(2).push(3).push(4).push(5);
 linkedList.toString();
 
 linkedList.pop();
@@ -9,20 +14,21 @@ linkedList.shift();
 linkedList.unshift(1);
 linkedList.toString();
 
-console.log(linkedList.get(1));
-linkedList.set(0, "Hello!");
+console.log(linkedList.get(0));
+linkedList.set(0, 0);
 linkedList.toString();
 
-linkedList.insert(1, "world");
-linkedList.insert(2, "!");
-linkedList.insert(3, "Bye!");
-linkedList.insert(4, "Bye!");
-linkedList.insert(4, "Bye!");
-linkedList.insert(0, "¡");
-linkedList.toString();
-linkedList.remove(0);
-linkedList.remove(linkedList.length - 1);
-linkedList.remove(linkedList.length - 2);
+linkedList.insert(1, 1);
 linkedList.toString();
 linkedList.reverse();
 linkedList.toString();
+
+/**
+ * DoublyLinkedList operations
+ */
+
+let doublyLinked = new DoublyLinkedList();
+console.log('DoublyLinkedList operations');
+doublyLinked.push(1).push(2).push(3).push(4).push(5);
+doublyLinked.toString();
+
