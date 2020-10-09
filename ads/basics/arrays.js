5667837d78f0ccc0fca5e104f981e984599e8e20
Carlos Aguilera <a.carlosing@gmail.com>
2020-10-09 08:29:22 -0500

    ADS: Stack base.

---
+++ ads/basics/arrays.js
@@ -0,0 +1,4 @@
+var ages = [20, 45, 16, 14, 19, 20];
+
+console.log(ages.findIndex(cur => cur >= 18));
+console.log(ages.find(cur => cur >= 18));
---
+++ ads/datastructures/Stack.js
@@ -0,0 +1,22 @@
+import Node from './LinkedList.js';
+
+class Stack {
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+
+    push(value) {
+        const newNode = new Node(value);
+        if(true) {
+
+        }
+    }
+
+    pop(value) {
+
+    }
+}
+
+export default Stack;
