b9f03b692b73aea139bc41dc4dd79b4490a792a0
Carlos Aguilera <a.carlosing@gmail.com>
2020-10-12 18:39:56 -0500

    ADS: MaxBinaryHeap - Extract Max.

--- javascript/ads/datastructures/MaxBinaryHeap.js
+++ javascript/ads/datastructures/MaxBinaryHeap.js
@@ -18,13 +18,57 @@ class MaxBinaryHeap {
             if (element <= parent)
                 break;
 
-            console.log(idx, parentidx);
             this.swap(idx, parentidx);
             idx = parentidx;
         }
     }
+
     swap(i, j) {
         [this.values[i], this.values[j]] = [this.values[j], this.values[i]];
+    }
+
+    extractMax() {
+        const max = this.values[0];
+        let end = this.values.pop();
+        if (this.values.length > 0) {
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return max;
+    }
+
+    sinkDown() {
+        let idx = 0;
+        const element = this.values[idx];
+        const length = this.values.length;
+        while (true) {
+            let [leftIdx, rightIdx] = this.getChildrenIdx(idx);
+            let left, right;
+            let swap = null;
+
+            if (leftIdx < length) {
+                left = this.values[leftIdx];
+                if (left > element)
+                    swap = leftIdx;
+            }
+
+            if (rightIdx < length) {
+                right = this.values[rightIdx];
+                if ((swap === null && right > element) || (swap !== null && right > left))
+                    swap = rightIdx;
+            }
+
+            if (swap === null)
+                break;
+
+            this.swap(idx, swap);
+            idx = swap;
+        }
+
+    }
+
+    getChildrenIdx(idx) {
+        return [2 * idx + 1, 2 * idx + 2]
     }
 }
 
--- javascript/ads/datastructures/main.js
+++ javascript/ads/datastructures/main.js
@@ -90,9 +90,13 @@ console.log(bst.DFSInOrder());
 
 let maxBinaryHeap = new MaxBinaryHeap();
 console.log('MaxBinaryHeap operations:');
-maxBinaryHeap.values = [41, 39, 33, 18, 27, 12];
+maxBinaryHeap.insert(41);
+maxBinaryHeap.insert(39);
+maxBinaryHeap.insert(33);
+maxBinaryHeap.insert(18);
+maxBinaryHeap.insert(27);
+maxBinaryHeap.insert(12);
 maxBinaryHeap.insert(55);
-maxBinaryHeap.insert(56);
-maxBinaryHeap.insert(54);
-maxBinaryHeap.insert(1);
+console.log(maxBinaryHeap.values);
+console.log(maxBinaryHeap.extractMax());
 console.log(maxBinaryHeap.values);
