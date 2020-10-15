198c5c68d9e91d6b7e433de818a08021ba3fd6a1
Carlos Aguilera <a.carlosing@gmail.com>
2020-10-14 20:19:00 -0500

    CJS: Promises.

--- javascript/cjs/async.js
+++ javascript/cjs/async.js
@@ -9,7 +9,7 @@ const first = () => {
     second();
     console.log("The end");
 }
-first();
+// first();
 
 
 function getrecipe() {
@@ -30,5 +30,39 @@ function getrecipe() {
 
     }, 1500);
 }
+// getrecipe();
 
-getrecipe();
+// ES6 - Promise: Pending, Settled/Resolved, Fullfilled, Rejected
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+const getIds = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (getRandomInt(10)) {
+            resolve([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+        } else {
+            reject("Server error.")
+        }
+    }, 1500);
+});
+
+const getRecipe = recipeId => {
+    return new Promise((resolve, rejected) => {
+        setTimeout((id) => {
+            const recipe = { title: "Fresh tomato", publisher: "Carlos" };
+            resolve(`${id}: ${recipe.title}`);
+        }, 1500, recipeId);
+    });
+}
+
+getIds
+    .then((recipes) => {
+        console.log(recipes);
+        return getRecipe(recipes[3]);
+    }).
+    then((recipe) => {
+        console.log(recipe);
+    })
+    .catch((error) => console.log(error));
