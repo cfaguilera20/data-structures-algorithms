a0a4b4ce2c5b80ef25aa7a7a7c10c45398f24c70
Carlos Aguilera <a.carlosing@gmail.com>
2020-10-21 18:00:59 -0500

    DOM: Chapter 2 - Document Nodes.

--- javascript/ads/algorithms/dijkstras.js
+++ javascript/ads/algorithms/dijkstras.js
@@ -1,3 +1,4 @@
+import PriorityQueue from '../datastructures/PriorityQueue.js';
 import WeightedGraph from '../datastructures/WeightedGraph.js';
 
 /**
@@ -38,3 +39,61 @@ import WeightedGraph from '../datastructures/WeightedGraph.js';
  * E        null
  * F        null
  */
+
+
+class Dijkstra extends WeightedGraph {
+    Dijkstra(start, finish) {
+        const nodes = new PriorityQueue();
+        const distances = {};
+        const previous = {};
+        let smallest;
+
+        for (let vertex in this.adjacencyList) {
+            if (vertex === start) {
+                distances[vertex] = 0;
+                nodes.enqueue(vertex, 0);
+            } else {
+                distances[vertex] = Infinity;
+                nodes.enqueue(vertex, Infinity);
+            }
+            previous[vertex] = null;
+        }
+
+        while (nodes.values.length) {
+            smallest = nodes.dequeue().value;
+            if (smallest === finish) {
+
+            }
+            if (smallest || distances[smallest] !== Infinity) {
+                for (let neighbor in this.adjacencyList[smallest]) {
+                    let nextNode = this.adjacencyList[smallest][neighbor];
+                    console.log(nextNode);
+                    nodes.enqueue(nextNode);
+                }
+            }
+        }
+
+    }
+}
+
+
+const graph = new Dijkstra();
+
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+
+graph.addEdge("A", "B", 4);
+graph.addEdge("A", "C", 2);
+graph.addEdge("B", "E", 3);
+graph.addEdge("C", "D", 2);
+graph.addEdge("C", "F", 4);
+graph.addEdge("D", "E", 3);
+graph.addEdge("D", "F", 1);
+graph.addEdge("E", "F", 1);
+console.log(graph.Dijkstra("A", "E"));
+
+
--- javascript/ads/datastructures/PriorityQueue.js
+++ javascript/ads/datastructures/PriorityQueue.js
@@ -5,6 +5,27 @@ class Node {
     }
 }
 
+// Small version
+// class PriorityQueue {
+//     constructor() {
+//         this.values = [];
+//     }
+
+//     enqueue(val, priority) {
+//         this.values.push({ val, priority });
+//         this.sort();
+//     }
+
+//     dequeue() {
+//         return this.values.shift();
+//     }
+
+//     // O(nlogn)
+//     sort() {
+//         this.values.sort((a, b) => a.priority - b.priority);
+//     }
+// }
+
 class PriorityQueue {
     constructor() {
         this.values = [];
