2d5e9ed84a21cc10dd2d858a8b2d91cafe8e60ef
Carlos Aguilera <a.carlosing@gmail.com>
2020-10-28 19:48:06 -0600

    ADV: Pure functions.

--- javascript/adv/functional.js
+++ javascript/adv/functional.js
@@ -11,6 +11,7 @@ function pop(arr) {
 pop(array);
 console.log(array);
 
+// Pure functions
 // Unmutable array
 const array2 = [1, 2, 3];
 function removeLastItem(arr) {
@@ -18,5 +19,31 @@ function removeLastItem(arr) {
     newArray.pop();
     return newArray;
 }
+
+function multiplyBy2(arr) {
+    return arr.map(item => item * 2);
+}
+
+// No side effects
 console.log(array2);
 console.log(removeLastItem(array2));
+console.log(multiplyBy2(array2));
+
+// Not pure function, affects the something outside
+function a() {
+    console.log('hi');
+}
+a();
+
+function b(num1, num2) {
+    return num1 + num2
+}
+console.log(b(3, 4));
+
+function c(num) {
+    return num * 2;
+}
+
+// Referential transparency
+console.log(c(b(3, 4)));
+console.log(c(7));
