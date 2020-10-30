e34d02f36819623262f2b6f921dbe164a6ec8557
Carlos Aguilera <a.carlosing@gmail.com>
2020-10-29 22:46:52 -0600

    ADS: HashTable.

--- javascript/ads/datastructures/HashTable.js
+++ javascript/ads/datastructures/HashTable.js
@@ -1,16 +1,77 @@
 class HashTable {
-    constructor() {
-
+    constructor(size = 53) {
+        this.keyMap = new Array(size);
     }
 
-    hash(key, arrayLen = 10) {
+    _hash(key) {
         let total = 0;
-        for(let char of key) {
+        let WEIRD_PRIME = 31;
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            let char = key[i];
             let value = char.charCodeAt(0) - 96;
-            total = (total + value) % arrayLen;
+            total = (total * WEIRD_PRIME + value) % this.keyMap.length;
         }
         return total;
     }
+
+    set(key, value) {
+        let index = this._hash(key);
+        if (!this.keyMap[index]) {
+            this.keyMap[index] = [];
+        }
+        this.keyMap[index].push([key, value]);
+    }
+
+    get(key) {
+        let index = this._hash(key);
+        if (this.keyMap[index]) {
+            for (let i = 0; i < this.keyMap[index].length; i++) {
+                let data = this.keyMap[index][i];
+                if (data[0] === key) {
+                    return data[1];
+                }
+            }
+        }
+        return undefined;
+    }
+    values() {
+        let values = [];
+        for (let i = 0; i < this.keyMap.length; i++) {
+            if (this.keyMap[i]) {
+                for (let j = 0; j < this.keyMap[i].length; j++) {
+                    if (!values.includes(this.keyMap[i][j][1])) {
+                        values.push(this.keyMap[i][j][1]);
+                    }
+                }
+            }
+        }
+        return values;
+    }
+    keys() {
+        let keys = [];
+        for (let i = 0; i < this.keyMap.length; i++) {
+            if (this.keyMap[i]) {
+                for (let j = 0; j < this.keyMap[i].length; j++) {
+                    if (!keys.includes(this.keyMap[i][j][0])) {
+                        keys.push(this.keyMap[i][j][0]);
+                    }
+                }
+            }
+        }
+        return keys;
+    }
 }
+
+let ht = new HashTable(2);
+ht.set("a", 1);
+ht.set("b", 2);
+ht.set("c", 3);
+ht.set("d", 4);
+ht.set("e", 5);
+ht.set("f", 5);
+console.log(ht.get('c'));
+console.log(ht.get('a'));
+console.log(ht.values());
+console.log(ht.keys());
 
 export default HashTable;
