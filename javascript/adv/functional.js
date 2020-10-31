10677d5989c9e5451da891b06581d8fe3a2721d9
Carlos Aguilera <a.carlosing@gmail.com>
2020-10-30 19:17:02 -0600

    ADV: Compose & Pipe.

--- javascript/adv/functional.js
+++ javascript/adv/functional.js
@@ -129,3 +129,23 @@ console.log('curriedMultiplyBy5', curriedMultiplyBy5(3));
 const multiply2 = (a, b, c) => a * b * c;
 const partialMultiplyBy5 = multiply2.bind(null, 5);
 console.log('partialMultiplyBy5', partialMultiplyBy5(4, 10));
+
+//------------------------------------------------
+// Compose: data -> fn -> data -> fn -> ...
+//------------------------------------------------
+
+const compose = (f, g) => (data) => f(g(data));
+const multiplyBy3 = (num) => num * 3;
+const makePositive = (num) => Math.abs(num);
+const multiplyBy3AndAbosulte = compose(multiplyBy3, makePositive);
+console.log('multiplyBy3AndAbosulte', multiplyBy3AndAbosulte(-50));
+
+//------------------------------------------------
+// Pipe
+//------------------------------------------------
+const pipe = (f, g) => (data) => g(f(data));
+
+// Diff
+fn1(fn2(fn3(50)));
+// compose(fn1, fn2, fn3)(50);
+// pipe(fn3, fn2, fn1)(50);
