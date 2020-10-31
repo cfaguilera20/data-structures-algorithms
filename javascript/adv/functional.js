505092af774546b676bc5c9497dbb9f2a7183bd5
Carlos Aguilera <a.carlosing@gmail.com>
2020-10-30 18:53:54 -0600

    ADV: Currying.

--- javascript/adv/functional.js
+++ javascript/adv/functional.js
@@ -116,3 +116,13 @@ console.log('incrementFn', incrementFn());
 //------------------------------------------------
 // Currying
 //------------------------------------------------
+
+const multiply = (a, b) => a * b;
+const curriedMultiply = (a) => (b) => a * b;
+const curriedMultiplyBy5 = curriedMultiply(5);
+console.log('curriedMultiply', curriedMultiply(5)(3));
+console.log('curriedMultiplyBy5', curriedMultiplyBy5(3));
+
+//------------------------------------------------
+// Partial Application
+//------------------------------------------------
