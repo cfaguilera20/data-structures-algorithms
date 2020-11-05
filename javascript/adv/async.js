6eae77e37377a6790414df9ac07bc7fbe054e155
Carlos Aguilera <a.carlosing@gmail.com>
2020-11-04 20:13:09 -0600

    ADV - Async parallel, race, sequence.

---
+++ javascript/adv/async.js
@@ -0,0 +1,33 @@
+// Parallel, sequencial, race
+const promisify = (item, delay) =>
+    new Promise((resolve) =>
+        setTimeout(() =>
+            resolve(item), delay));
+
+
+const a = () => promisify('a', Math.random() * 1000);
+const b = () => promisify('b', Math.random() * 1000);
+const c = () => promisify('c', Math.random() * 1000);
+
+async function parallel() {
+    const promises = [a(), b(), c()];
+    const [out1, out2, out3] = await Promise.all(promises);
+    return `Parallel is done: ${out1} ${out2} ${out3}`;
+}
+parallel().then(console.log);
+
+async function race() {
+    const promises = [a(), b(), c()];
+    const out1 = await Promise.race(promises);
+    return `Race is done: ${out1} `;
+}
+race().then(console.log);
+
+async function sequence() {
+    const out1 = await a();
+    const out2 = await b();
+    const out3 = await c();
+
+    return `Sequence is done: ${out1} ${out2} ${out3}`;
+}
+sequence().then(console.log);
