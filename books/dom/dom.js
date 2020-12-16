5ec1bd6001f9cf323a7c40508436f3d0238ea2a9
Carlos Aguilera <a.carlosing@gmail.com>
2020-12-15 22:11:25 -0600

    DOM - dom.js

--- books/dom/dom.js
+++ books/dom/dom.js
@@ -2,12 +2,60 @@
     const global = win;
     const doc = this.document;
     const dom = function (params, context) {
-        return GetOrMakeDom(params, context);
+        return new GetOrMakeDom(params, context);
     };
+    const regXContainsTag = /^\s*<(\w+|!)[^>]*>/;
     const GetOrMakeDom = function (params, context) {
+        let currentContext = doc;
+        if (context) {
+            if (context.nodetype) {
+                currentContext = context;
+            } else {
+                currentContext = doc.querySelector(context);
+            }
+        }
 
+        if (!params || params === '' || typeof params === 'string' && params.trim() === '') {
+            this.length = 0;
+            return this;
+        }
+
+        if (typeof params === 'string' && regXContainsTag.test(params)) {
+            const divElem = currentContext.createElement('div');
+            divElem.className = 'hippo-doc-frag-wrapper';
+            const docFrag = currentContext.createDocumentFragment();
+            docFrag.appendChild(divElem);
+            const queryDiv = docFrag.querySelector('div');
+            queryDiv.innerHTML = params;
+            const numberOfChildren = queryDiv.children.length;
+            for (let z = 0; z < numberOfChildren; z++) {
+                this[z] = queryDiv.children[z];
+            }
+            this.length = numberOfChildren;
+            return this;
+        }
+
+        if (typeof params === 'object' && params.nodeName) {
+            this.length = 1;
+            this[0] = params;
+            return this;
+        }
+
+        let nodes;
+        if (typeof params !== 'string') {
+            nodes = params;
+        } else {
+            nodes = currentContext.querySelector(params.trim());
+        }
+
+        const nodeLength = nodes.length;
+        for (let i = 0; i < nodeLength; i++) {
+            this[i] = nodes[i];
+        }
+        this.length = nodeLength;
+        return this;
     };
 
     global.dom = dom;
-    dom.fn = GetOrMakeDom.prototype;
+    dom.fn = GetOrMakeDom.prototype
 })(window);
