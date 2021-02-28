8c4ecfe699aac29fdbc4c25734780a42a486effa
Carlos Aguilera <a.carlosing@gmail.com>
2021-02-28 16:31:38 -0600

    Create BalancedBrackets.js

---
+++ platforms/hackerrank/BalancedBrackets.js
@@ -0,0 +1,22 @@
+/**
+ * https://www.hackerrank.com/challenges/balanced-brackets/problem
+ */
+function isBalanced(s) {
+    let charsDict = {'[': ']', '{': '}', '(': ')'};
+    let stack = [];
+    let i = 0;
+    while(i < s.length) {
+        let char = s[i];
+        
+        if(charsDict[char]) {
+            stack.push(char);
+        } else {
+           let open = stack.pop();
+           if(charsDict[open] !== char) {
+               return 'NO';
+           }     
+        }
+        i++
+    }
+    return 'YES';
+}
