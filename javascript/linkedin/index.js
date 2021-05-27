1d73978ea26fff8b8e9c0149dbe4c4f25059e987
Carlos Aguilera <a.carlosing@gmail.com>
2021-05-27 12:15:06 -0500

    Js.

--- javascript/linkedin/index.js
+++ javascript/linkedin/index.js
@@ -28,3 +28,45 @@ function bigBrother() {
 
 // Before running this code, what do you think the output is?
 console.log(bigBrother());
+
+// Function declaration - Hoisted
+function canada() {
+    console.log(`cold!`);
+}
+
+// Function expression - Hoisted as undefined
+var india = () => {
+    console.log(`warm!`);
+};
+
+// Function Invocation/Call/Execution
+canada();
+india();
+
+// Function arguments
+function marry(person1, person2) {
+    console.log('Arguments', arguments);
+    console.log('Arguments array from', Array.from(arguments));
+    console.log(`${person1} is married to ${person2}`);
+}
+
+// Function spread arguments
+function marry(...spreadArgs) {
+    console.log('Arguments', spreadArgs);
+    console.log('Arguments array from', Array.from(spreadArgs));
+    console.log(`${spreadArgs[0]} is married to ${spreadArgs[1]}`);
+}
+
+marry('Dog', 'Cat');
+
+// Variable environment
+function two() {
+    var isValid;
+}
+
+function one() {
+    var isValid = true;
+}
+
+var isValid = false;
+one();
