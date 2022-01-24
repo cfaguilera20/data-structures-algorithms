4aceff71d12ab69feb599075b63784b3d0b3d535
Carlos Aguilera <a.carlosing@gmail.com>
2022-01-24 11:51:06 -0600

    Ruby: numbers.

--- ruby/numbers_bool.rb
+++ ruby/numbers_bool.rb
@@ -42,3 +42,36 @@ section("Between:")
 p 10.between?(5, 20)
 p "cat".between?("ant", "dog")
 p "gnu".between?("ant", "dog")
+
+
+section("Float methods:")
+puts 10.0.class
+puts 10.5.floor
+puts 10.5.ceil
+puts 3.14159.round
+puts 3.14159.round(3)
+
+
+section("Times:")
+3.times { puts "Hello, charls" }
+3.times do
+    puts "Hello, charls"
+end
+3.times { |i| puts "Hello, charls #{i}" }
+3.times do |i|
+    puts "Hello, charls #{i}"
+end
+
+
+section("Downto:")
+5.downto(1) { |i| puts "Hello, charls #{i}" }
+5.downto(1) do |i|
+    puts "Hello, charls #{i}"
+end
+
+
+section("Upto:")
+5.upto(10) { |i| puts "Hello, charls #{i}" }
+5.upto(10) do |i|
+    puts "Hello, charls #{i}"
+end
